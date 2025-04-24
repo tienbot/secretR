fcecc0b027c764d6e831120829537b702ee4d369
Tienbot <tien.ru@mail.ru>
2025-04-24 21:26:35 +0300

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-console.log(process.env.REACT_APP_MY_SECRET)
+console.log("Мой секрет:", process.env.REACT_APP_MY_SECRET);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
